f1fde1f4d175d2b1429cdc845ab5db92224b2f92
dunika <dbreen91@gmail.com>
2017-10-19 21:10:38 +0100

    Updated config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,21 @@
-console.log(JSON.stringify(process.env));
-module.exports = {
-  baseUrl: process.env.BASE_URL || 'http://localhost:3000',
-  cvLibraryApi: 'http://www.cv-library.co.uk/cgi-bin/feed.xml?affid=102765&roi=1',
-  createPost: {
-    url: process.env.CREATE_POST_URL || 'http://localhost/wp-admin/admin-ajax.php',
-    action: process.env.CREATE_POST_ACTION || 'post_job',
-  },
-  database: process.env.MONGODB_URI || 'mongodb://localhost/your-future-self-test',
-};
+if (process.env.NODE_ENV !== 'production') {
+  module.exports = {
+    baseUrl: 'http://localhost:3000',
+    cvLibraryApi: 'http://www.cv-library.co.uk/cgi-bin/feed.xml?affid=102765&roi=1',
+    createPost: {
+      url: 'http://localhost/wp-admin/admin-ajax.php',
+      action: 'post_job',
+    },
+    database: 'mongodb://localhost/your-future-self-test',
+  };
+} else {
+  module.exports = {
+    baseUrl: process.env.BASE_URL,
+    cvLibraryApi: 'http://www.cv-library.co.uk/cgi-bin/feed.xml?affid=102765&roi=1',
+    createPost: {
+      url: process.env.CREATE_POST_URL,
+      action: process.env.CREATE_POST_ACTION,
+    },
+    database: process.env.MONGODB_URI,
+  };
+}
