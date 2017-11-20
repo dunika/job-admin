dad52901329ff41abcabaaa500912dcdf421d475
dunika <dbreen91@gmail.com>
2017-11-20 20:20:24 +0000

    Adding settings api

--- src/server/api.js
+++ src/server/api.js
@@ -6,16 +6,16 @@ import { api as cvLibrary } from 'server/modules/cv-library';
 import { api as indeed } from 'server/modules/indeed';
 import { api as scrape } from 'server/modules/scraper';
 import { apiErrorHandler } from 'server/middleware';
-import { api as facebook } from 'server/facebook';
+import { api as settings } from 'server/modules/settings';
 
 export default () => {
   const api = Router();
 
   api.use('/', job());
+  api.use('/', settings());
   api.use('/', scrape());
   api.use('/', indeed());
   api.use('/', cvLibrary());
-  api.use('/', facebook());
   api.use('/', wordpress());
 
   api.use(apiErrorHandler());
